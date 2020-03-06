f8d1759fe008ef75ee6ba50ef1974e2dac6b6bf9
giuliafrascaria <giulia.frascaria@gmail.com>
2020-03-06 15:43:35 +0000

    guess what that didn't work xD

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -413,7 +413,7 @@ static const struct bpf_func_proto bpf_kstrtol_proto = {
 	.func		= bpf_kstrtol,
 	.gpl_only	= true,
 	.ret_type	= RET_INTEGER,
-	.arg1_type	= ARG_ANYTHING,
+	.arg1_type	= ARG_PTR_TO_MEM,
 	.arg2_type	= ARG_CONST_SIZE,
 	.arg3_type	= ARG_PTR_TO_LONG,
 };
